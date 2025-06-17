d36cb73ed87bc7d016309d3d890fc77e8b32c76e
blakeaw <blakeaw1102@gmail.com>
2025-06-17 14:04:41 -0500

    Added the expressions and rules contexts.

--- qspy/contexts.py
+++ qspy/contexts.py
@@ -1,4 +1,6 @@
 from contextlib import contextmanager
+import sympy
+import pysb
 from base import ComponentContext
 import core
 from pysb.units.core import check as units_check
@@ -7,6 +9,8 @@ __all__ = [
     "parameters",
     "compartments",
     "monomers",
+    "expressions",
+    "rules",
     # "units",
 ]
 
@@ -18,11 +22,14 @@ __all__ = [
 #     finally:
 #         pass
 
+
 class parameters(ComponentContext):
     component_name = "parameter"
 
     @staticmethod
     def _validate_value(name, val):
+        if isinstance(val, sympy.Expr):
+            return (val, None)
         if not isinstance(val, tuple) or len(val) != 2:
             raise ValueError(f"Parameter '{name}' must be a tuple: (value, unit)")
         value, unit = val
@@ -34,6 +41,9 @@ class parameters(ComponentContext):
 
     @staticmethod
     def create_component(name, value, unit):
+        if isinstance(value, sympy.Expr):
+            expr = core.Expression(name, value)
+            return expr
         param = core.Parameter(name, value, unit=unit)
         return param
 
@@ -53,13 +63,14 @@ class compartments(ComponentContext):
             raise ValueError(
                 f"Compartment size for '{name}' must be a Parameter or constant expression"
             )
-        return (size, )
+        return (size,)
 
     @staticmethod
     def create_component(name, size):
-        #print(size)
+        # print(size)
         compartment = core.Compartment(name, size=size)
         return compartment
+
 
 class monomers(ComponentContext):
     component_name = "monomer"
@@ -70,12 +81,65 @@ class monomers(ComponentContext):
             raise ValueError(f"Monomer '{name}' must be a tuple: (sites, site_states)")
         sites, site_states = val
         if (sites is not None) and (not isinstance(sites, list)):
-            raise ValueError(f"Monomer sites value for '{name}' must be a list of site names")
+            raise ValueError(
+                f"Monomer sites value for '{name}' must be a list of site names"
+            )
         if (site_states is not None) and (not isinstance(site_states, dict)):
-            raise ValueError(f"Monomer site_states for '{name}' must be a dictionary of sites and their states")
+            raise ValueError(
+                f"Monomer site_states for '{name}' must be a dictionary of sites and their states"
+            )
         return (sites, site_states)
 
     @staticmethod
     def create_component(name, sites, site_states):
         monomer = core.Monomer(name, sites, site_states)
         return monomer
+
+
+class expressions(ComponentContext):
+    component_name = "expression"
+
+    @staticmethod
+    def _validate_value(name, val):
+        if not isinstance(val, sympy.Expr):
+            raise ValueError(f"Expression '{name}' must be a sympy.Expr")
+        return (val,)
+
+    @staticmethod
+    def create_component(name, expr):
+        expression = core.Expression(name, expr)
+        return expression
+
+
+class rules(ComponentContext):
+    component_name = "rule"
+
+    @staticmethod
+    def _validate_value(name, val):
+        if not isinstance(val, tuple) or (len(val) < 2 or len(val) > 3):
+            raise ValueError(
+                f"Rule '{name}' input must be a tuple: (RuleExpression, rate_forward) if irreversible or (RuleExpression, rate_forward, rate_reverse) if reversible"
+            )
+        if len(val) == 2:
+            rxp, rate_forward = val
+            rate_reverse = None
+        elif len(val) == 3:
+            rxp, rate_forward, rate_reverse = val
+        if not isinstance(rxp, pysb.RuleExpression):
+            raise ValueError(f"Rule '{name}' must contain a valid RuleExpression")
+        if not isinstance(rate_forward, (core.Parameter, core.Expression)):
+            raise ValueError(
+                f"rate_forward value for '{name}' must be a Parameter or Expression"
+            )
+        if (rate_reverse is not None) and not isinstance(
+            rate_forward, (core.Parameter, core.Expression)
+        ):
+            raise ValueError(
+                f"rate_reverse value for '{name}' must be a Parameter or Expression"
+            )
+        return (rxp, rate_forward, rate_reverse)
+
+    @staticmethod
+    def create_component(name, rxp, rate_forward, rate_reverse):
+        rule = core.Rule(name, rxp, rate_forward, rate_reverse)
+        return rule
